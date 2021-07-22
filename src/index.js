f0fe71cce72cea943353323a416b5b2827a84beb
iamfotx <iamfotx@gmail.com>
2021-07-22 15:44:14 +0500

    Setup ESBuild (#4)

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {add} from './math'
+
+const result = add(1, 2);
+
+console.log('result', result)
+console.log('API_URL: ', process.env.API_URL)
---
+++ src/math.js
@@ -0,0 +1,7 @@
+export function add(num1, num2) {
+	return num1 + num2;
+}
+
+export function multiply(num1, num2) {
+	return num1 * num2;
+}
