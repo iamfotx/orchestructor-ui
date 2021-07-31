09040771395a1131aadcc3e4de2e3dcb2a0b3f3a
iamfotx <iamfotx@gmail.com>
2021-08-01 04:54:37 +0500

    setup tailwind

--- src/header.tsx
+++ src/header.tsx
@@ -1,7 +1,5 @@
 export function Header(): JSX.Element {
   return (
-    <>
-      <h1>Orchestructor UI :)</h1>
-    </>
-  );
+    <h1 className="font-bold font-serif m-2">Orchestructor UI</h1>
+  )
 }
